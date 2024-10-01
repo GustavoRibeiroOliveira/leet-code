b317aa8ebe24273112cfb6fcb0e0fc74aaad281b
Gustavo Ribeiro de Oliveira <gustavo.r@inatel.com>
2024-10-01 09:02:10 -0300

    3. Longest Substring Without Repeating Characters

---
+++ 3.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        length = len(s)
+        repeatedCharacters = []
+        maxLength = 0
+        tempMaxLength = 0
+        i = 0
+        while i < len(s):
+            if s[i] in repeatedCharacters:
+                repeatedCharacters = []
+                if tempMaxLength > maxLength:
+                    maxLength = tempMaxLength
+                i -= tempMaxLength
+                tempMaxLength = 0
+            else:
+                tempMaxLength += 1
+                repeatedCharacters.append(s[i])
+            i += 1
+        if tempMaxLength > maxLength:
+            maxLength = tempMaxLength
+        return maxLength
+
+teste = Solution()
+print(teste.lengthOfLongestSubstring("anviaj"))
