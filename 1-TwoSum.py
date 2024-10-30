dc82b55299ba773486653d0f9e673b3993930fbe
Gustavo Ribeiro de Oliveira <gustavo.r@inatel.com>
2024-10-30 08:38:53 -0300

    1. Two Sum

---
+++ 1-TwoSum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums, target):
+        n = len(nums)
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
+
+teste = Solution()
+print(teste.twoSum(nums = [2,7,11,15], target = 9))
