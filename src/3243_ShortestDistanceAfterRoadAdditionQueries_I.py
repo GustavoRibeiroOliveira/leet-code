8b2f7002d46bb393b559f57a60fff86a6e31c101
Gustavo Ribeiro de Oliveira <gustavo.r@inatel.com>
2024-12-03 09:25:09 -0300

    3243. Shortest Distance After Road Addition Queries 1

---
+++ src/3243_ShortestDistanceAfterRoadAdditionQueries_I.py
@@ -0,0 +1,9 @@
+class Solution:
+    def shortestDistanceAfterQueries(
+        self, n: int, queries: list[list[int]]
+    ) -> list[int]:
+        pass
+
+
+teste = Solution()
+print(teste.shortestDistanceAfterQueries(n=5, queries=[[2, 4], [0, 2], [0, 4]]))
